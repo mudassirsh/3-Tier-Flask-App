0478ac1365a8c36435d7760da69d9001e2bb6b2d
Mudassir Sheikh <mudassirs@opentext.com>
2023-12-27 03:23:09 +0000

    updated

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,101 +1,4 @@
 
-// import { useEffect, useState } from 'react';
-
-
-// function App() {
-//   const [users, setUsers] = useState([]);
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [email, setEmail] = useState('');
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-  
-//   /* Get API */
-//   const fetchData = async () => {
-//     try {
-//       const apiUrl = 'http://${window.location.hostname}:5000/get_list';
-//       const response = await fetch(apiUrl);
-//       const jsonData = await response.json();
-  
-//       console.log('API Response:', jsonData);
-  
-//       if (jsonData.Customers && Array.isArray(jsonData.Customers)) {
-//         // Assuming jsonData.Customers is an array
-//         setUsers(jsonData.Customers);
-//       } else {
-//         console.error('Invalid data format:', jsonData);
-//       }
-//     } catch (error) {
-//       console.error('Error', error);
-//     }
-//   };
-  
-//  /* Post API */
-//   const handleRegister = async () => {
-//     try {
-//       const response = await fetch('http://${window.location.hostname}:5000/create', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           username,
-//           password,
-//           email,
-//         }),
-//       });
-
-//       const result = await response.json();
-//       console.log('Registration Result:', result);
-
-//       // After successful entry, fetch updated user data
-//       fetchData();
-//     } catch (error) {
-//       console.error('Error during registration:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="App-header">
-//           <h1>Enter User Details</h1>
-//       <label>
-//         Username:
-//         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-//       </label>
-//       <label>
-//         Password:
-//         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//       </label>
-//       <label>
-//         Email:
-//         <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-//       </label>
-//       <button onClick={handleRegister}>Register</button>
-//       <h1>Customer List</h1>
-//       <ul>
-//         {users.map((customer, index) => (
-//           <li key={index}>
-//             <strong>Username:</strong> {customer.username},{' '}
-//             <strong>Email:</strong> {customer.email},{' '}
-//             <strong>Password:</strong> {customer.password}
-//           </li>
-//         ))}
-//       </ul>
-
-  
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-//########################
-
-
 import { useEffect, useState } from 'react';
 
 
@@ -112,7 +15,7 @@ function App() {
   /* Get API */
   const fetchData = async () => {
     try {
-      const apiUrl = `http://${window.location.hostname}:5000/get_list`;
+      const apiUrl = `http://${window.location.hostname}:80/get_list`;
       const response = await fetch(apiUrl);
       const jsonData = await response.json();
   
@@ -132,8 +35,7 @@ function App() {
  /* Post API */
   const handleRegister = async () => {
     try {
-        
-        const response = await fetch(`http://${window.location.hostname}:5000/create`, {
+      const response = await fetch(`http://${window.location.hostname}:80/create`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -188,3 +90,5 @@ function App() {
 }
 
 export default App;
+
+
